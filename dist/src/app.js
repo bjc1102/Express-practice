a7ffbc32f894558ea97c781e972eefdcf8c72b82
byung ju choi <bjc30849@gmail.com>
2022-02-28 16:17:27 +0900

    DB 테스트

--- dist/src/app.js
+++ dist/src/app.js
@@ -1,17 +1,19 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
+'use strict';
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
+Object.defineProperty(exports, '__esModule', { value: true });
+const express_1 = __importDefault(require('express'));
 const app = (0, express_1.default)();
-app.get("/welcome", (req, res, next) => {
-    res.send("welcome!");
+app.get('/welcome', (req, res, next) => {
+  res.send('welcome!');
 });
-app.listen("1234", () => {
-    console.log(`
+app.listen('8080', () => {
+  console.log(`
   ################################################
-  🛡️  Server listening on port: 1234🛡️
+  🛡️  Server listening on port: 8080
   ################################################
 `);
 });
--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,34 @@
-import express, { Request, Response, NextFunction } from "express";
+import express, { Request, Response, NextFunction } from 'express';
+import mysql from 'mysql';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
 
-app.get("/welcome", (req: Request, res: Response, next: NextFunction) => {
-  res.send("welcome!");
+const PORT = Number(process.env.DB_PORT) || 4000;
+
+const con = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: process.env.DB_PASSWORD,
+  port: PORT,
 });
 
-app.listen("1234", () => {
+con.connect(function (err) {
+  if (err) throw err;
+  console.log('Connected');
+});
+
+app.get('/welcome', (req: Request, res: Response, next: NextFunction) => {
+  res.send('welcome!');
+});
+
+app.get('/', (req, res) => res.send('Hellow World!!'));
+
+app.listen('8080', () => {
   console.log(`
   ################################################
-  🛡️  Server listening on port: 1234🛡️
+  🛡️  Server listening on port: 8080
   ################################################
 `);
 });
