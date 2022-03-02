be9f0ee8b322b33b2a531eb1711c99e6136f5097
byung ju choi <bjc30849@gmail.com>
2022-03-02 15:12:40 +0900

    feat: jwt 토큰 생성 및 확인

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,14 @@
 import express, { Request, Response, NextFunction, response } from 'express';
 import mysql from 'mysql';
 import dotenv from 'dotenv'; //DB 패스워드와 비밀번호드을 숨기기 위해 사용함
+import jwt from 'jsonwebtoken';
+import { decode } from 'punycode';
 dotenv.config();
 
 const app = express();
 
 const PORT = Number(process.env.DB_PORT) || 3000;
+const JWT_KEY = process.env.JWT_KEY || 'key';
 
 const con = mysql.createConnection({
   host: 'localhost',
@@ -44,7 +47,12 @@ app.get('/welcome', (req: Request, res: Response, next: NextFunction) => {
 
 app.get('/', (req, res) => {
   // const sql = 'insert into 학생(학번 , 이름 , 학년 , 학과) values (101, "영수나", 4 , "전자공")';
-  const sql = 'select * from 학생';
+  const sql1 = 'update 학생 set 학년 = 2 where 학과 = "컴퓨터"';
+  const sql = 'select 학생.이름, 학생.학번 from 학생 where 학과="컴퓨터"';
+  con.query(sql1, function (err, result) {
+    if (err) throw err;
+    console.log('update complete');
+  });
   con.query(sql, function (err, result) {
     if (err) throw err;
     res.send(result);
@@ -59,6 +67,20 @@ app.get('/all', (req, res) => {
   });
 });
 
+app.get('/token', (req, res) => {
+  const Token = jwt.sign(
+    {
+      data: 'foobar',
+    },
+    JWT_KEY,
+    { expiresIn: 60 * 60 }
+  );
+
+  const decoded = jwt.verify(Token, JWT_KEY);
+  console.log(decoded);
+  res.send(Token);
+});
+
 app.listen('8080', () => {
   console.log(`
   ################################################
@@ -66,3 +88,7 @@ app.listen('8080', () => {
   ################################################
 `);
 });
+
+// delete는 참조 무결성 문제를 고려해야함
+// 관계형 데이터베이스에서 데이터를 삭제할 때 미치는 영향
+// table 생성시 외래키로 분류한 것들
